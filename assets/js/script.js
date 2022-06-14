462208a14f13a47961c1af8c7872bd007860f06a
bethanyshoun <bethanyshoun@gmail.com>
2022-06-14 19:06:13 -0400

    added moment

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,30 @@
-/*
+
+var myDate = moment().format("MMMM Do YYYY");
+
+    console.log(myDate);
+
+
+moment();
+
+console.log(moment())
+console.log("Anything")
+
+$("#currentDay").text(myDate);
+
+//$(document).ready(function() {
+  //  $("#currentDay").text.(moment().format("dddd, MMMM Do"));
+
+//});
+
+//var moment = require('moment');
+//var today = moment();
+//console.log(today.format());
 //Not working yet
-$(document).ready(function() {
- $("#currentDay").text(moment().format('dddd, MMMM Do,'));
-    console.log(date);
-});
-*/
+//$("#currentDay").ready(function() {
+//$("#currentDay").text(moment().format('dddd, MMMM Do'));
+//});
+//today ();
+
+//var endDate = moment();
+
+//endDate();
