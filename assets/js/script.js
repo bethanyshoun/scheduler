bcef5ffe365e36336f75b33fe78a60cf292da1af
bethanyshoun <bethanyshoun@gmail.com>
2022-07-09 11:58:04 -0400

    clean up JS code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,10 @@
-
+//get current date in header using Moment.js
 var myDate = moment().format("MMMM Do YYYY");
 
     console.log(myDate);
-
-
 moment();
-
 console.log(moment())
 console.log("Anything")
-
 $("#currentDay").text(myDate);
 
-//$(document).ready(function() {
-  //  $("#currentDay").text.(moment().format("dddd, MMMM Do"));
 
-//});
-
-//var moment = require('moment');
-//var today = moment();
-//console.log(today.format());
-//Not working yet
-//$("#currentDay").ready(function() {
-//$("#currentDay").text(moment().format('dddd, MMMM Do'));
-//});
-//today ();
-
-//var endDate = moment();
-
-//endDate();
