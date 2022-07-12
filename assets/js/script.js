3e93c35a97390b0fda77b2a50994f1ea068cdb74
bethanyshoun <bethanyshoun@gmail.com>
2022-07-11 23:49:44 -0400

    cleaned up classes and IDs in HTML, added click functionality and local storage set/get for saving and persistence

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,58 @@
 //get current date in header using Moment.js
-var myDate = moment().format("MMMM Do YYYY");
-
-    console.log(myDate);
+var myDate = moment().format("dddd, MMMM Do YYYY");
 moment();
-console.log(moment())
-console.log("Anything")
 $("#currentDay").text(myDate);
+
+//Save button functionality
+$(document).ready(function () {
+  // event listener for Save Button
+  $(".saveBtn").on("click", function () {
+    var text = $(this).siblings(".task").val();
+    var time = $(this).parent().attr("id");
+
+    localStorage.setItem(time, text);
+
+    function checkTime () {
+
+      //get current time
+      var currentTime = moment().hour();
+
+      //check each time block
+      $(".time-block").each(function () {
+        var timeBlock = parseInt($(this).attr("id").split("hour") [1]);
+
+        if (timeBlock < currentTime) {
+          $(this).removeClass("future");
+          $(this).removeClass("present");
+          $(this).addClass("past");
+        }
+        else if (timeBlock === currentTime) {
+          $(this).removeClass("past");
+          $(this).removeClass("future");
+          $(this).addClass("present");
+        }
+        else {
+          $(this).removeClass("present");
+          $(this).removeClass("past");
+          $(this).addClass("future");
+        }
+      })
+      
+    }
+    checkTime();
+  })
+  // Retrieve data from local storage if applicable
+$("#9am .task").val(localStorage.getItem("9am"));
+$("#10am .task").val(localStorage.getItem("10am"));
+$("#11am .task").val(localStorage.getItem("11am"));
+$("#12pm .task").val(localStorage.getItem("12pm"));
+$("#1pm .task").val(localStorage.getItem("1pm"));
+$("#2pm .task").val(localStorage.getItem("2pm"));
+$("#3pm .task").val(localStorage.getItem("3pm"));
+$("#4pm .task").val(localStorage.getItem("4pm"));
+$("#5pm .task").val(localStorage.getItem("5pm"));
+
+
+})
 
 
